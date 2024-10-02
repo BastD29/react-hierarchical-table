73c22e6859febda75f96ebd538739a79c2b3b7a2
BastD29 <bastiendaniel9@gmail.com>
2024-10-02 17:29:55 +0200

    set name field as row header

--- src/components/GridExample4.tsx
+++ src/components/GridExample4.tsx
@@ -1,6 +1,6 @@
 import {
-  // ColumnDirective,
-  // ColumnsDirective,
+  ColumnDirective,
+  ColumnsDirective,
   TreeGridComponent,
 } from "@syncfusion/ej2-react-treegrid";
 import data from "../data/Sectorisation.json";
@@ -13,48 +13,18 @@ const GridExample4 = () => {
     <div>
       <TreeGridComponent
         dataSource={sectorData.data.roots}
-        treeColumnIndex={1}
-        childMapping="children"
-      />
-
-      {/* <TreeGridComponent
-        dataSource={sectorData.data.roots}
-        treeColumnIndex={1}
+        treeColumnIndex={0}
         childMapping="children"
       >
         <ColumnsDirective>
-          <ColumnDirective
-            field="id"
-            headerText="ID"
-            width="100"
-            textAlign="Left"
-          />
           <ColumnDirective
             field="name"
             headerText="Name"
             width="150"
             textAlign="Left"
           />
-          <ColumnDirective
-            field="type"
-            headerText="Type"
-            width="150"
-            textAlign="Left"
-          />
-          <ColumnDirective
-            field="external_reference"
-            headerText="Reference"
-            width="150"
-            textAlign="Left"
-          />
-          <ColumnDirective
-            field="model_code"
-            headerText="Model Code"
-            width="150"
-            textAlign="Left"
-          />
         </ColumnsDirective>
-      </TreeGridComponent> */}
+      </TreeGridComponent>
     </div>
   );
 };
