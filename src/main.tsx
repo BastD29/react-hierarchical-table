b1f72c0d428f57f5c28345fde5b7b7f31f0c07e8
BastD29 <bastiendaniel9@gmail.com>
2024-10-02 15:12:26 +0200

    free trial license key setting - part I (waiting for key)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from "./components/App";
 import "./style/index.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import "ag-grid-enterprise";
+import { LicenseManager } from "ag-grid-enterprise";
+
+LicenseManager.setLicenseKey("AG Grid Enterprise");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
