a1a70a45c6a0a1d103e51f09c08f9538f9c3813e
BastD29 <bastiendaniel9@gmail.com>
2024-10-02 16:25:25 +0200

    refactor GridExample3

--- src/components/GridExample3.tsx
+++ src/components/GridExample3.tsx
@@ -1,4 +1,8 @@
-import { TreeGridComponent } from "@syncfusion/ej2-react-treegrid";
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  TreeGridComponent,
+} from "@syncfusion/ej2-react-treegrid";
 import { sortData } from "../data/sortData";
 
 const GridExample3 = () => {
@@ -9,6 +13,37 @@ const GridExample3 = () => {
         treeColumnIndex={1}
         childMapping="subtasks"
       />
+
+      {/* <TreeGridComponent
+        dataSource={sortData}
+        treeColumnIndex={1}
+        childMapping="subtasks"
+      >
+        <ColumnsDirective>
+          <ColumnDirective field="Category" headerText="Category" width="150" />
+          <ColumnDirective
+            field="orderName"
+            headerText="Order Name"
+            width="170"
+          />
+          <ColumnDirective
+            field="orderDate"
+            headerText="Order Date"
+            width="130"
+            format="yMd"
+            textAlign="Right"
+            type="date"
+          />
+          <ColumnDirective
+            field="price"
+            headerText="Price"
+            width="100"
+            textAlign="Right"
+            type="number"
+            format="C0"
+          />
+        </ColumnsDirective>
+      </TreeGridComponent> */}
     </div>
   );
 };
